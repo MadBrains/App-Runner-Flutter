dcc320fb7e378f65d682e1efa00335866e101df6
MiT <ilnar.miftakhutdinov@gmail.com>
2022-11-01 15:27:21 +0400

    fix: exports

--- lib/app_runner.dart
+++ lib/app_runner.dart
@@ -12,4 +12,5 @@ export 'src/app_runner.dart'
         InitializeBinding,
         InitializeFunctions,
         OnError,
-        ErrorWidgetBuilder;
+        RenderObjectBuilder,
+        ErrorRenderObjectBuilder;
